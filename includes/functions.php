9053a4d31689c6b40a17cbec6fc2a00f5d02b8b9
Younes DRO <dro66_8@hotmail.fr>
2022-12-16 10:50:13 +0100

    Add utils functions

--- includes/functions.php
+++ includes/functions.php
@@ -21,3 +21,70 @@ function ets_profilepress_discord_check_saved_settings_status() {
 
 		return $status;
 }
+
+/**
+ * Get WP pages list.
+ *
+ * @param INT $ets_profilepress_discord_redirect_page_id The Page ID.
+ *
+ * @return STRING $options Html select options.
+ */
+function ets_profilepress_discord_pages_list( $ets_profilepress_discord_redirect_page_id ) {
+	$args    = array(
+		'sort_order'   => 'asc',
+		'sort_column'  => 'post_title',
+		'hierarchical' => 1,
+		'exclude'      => '',
+		'include'      => '',
+		'meta_key'     => '',
+		'meta_value'   => '',
+		'exclude_tree' => '',
+		'number'       => '',
+		'offset'       => 0,
+		'post_type'    => 'page',
+		'post_status'  => 'publish',
+	);
+	$pages   = get_pages( $args );
+	$options = '<option value="">-</option>';
+	foreach ( $pages as $page ) {
+		$selected = ( esc_attr( $page->ID ) === $ets_profilepress_discord_redirect_page_id ) ? ' selected="selected"' : '';
+		$options .= '<option data-page-url="' . ets_get_profilepress_discord_formated_discord_redirect_url( $page->ID ) . '" value="' . esc_attr( $page->ID ) . '" ' . $selected . '> ' . sanitize_text_field( $page->post_title ) . ' </option>';
+	}
+
+	return $options;
+}
+
+/**
+ * Get formated redirect url.
+ *
+ * @param INT $page_id
+ *
+ * @return STRING $url
+ */
+function ets_get_profilepress_discord_formated_discord_redirect_url( $page_id ) {
+	$url = esc_url( get_permalink( $page_id ) );
+
+	$parsed = parse_url( $url, PHP_URL_QUERY );
+	if ( $parsed === null ) {
+		return $url .= '?via=connect-profilepress-discord-addon';
+	} else {
+		if ( stristr( $url, 'via=connect-profilepress-discord-addon' ) !== false ) {
+			return $url;
+		} else {
+			return $url .= '&via=connect-profilepress-discord-addon';
+		}
+	}
+}
+
+/**
+ * Get current screen URL
+ *
+ * @param NONE
+ * @return STRING $url
+ */
+function ets_profilepress_discord_get_current_screen_url() {
+	$parts       = parse_url( home_url() );
+	$current_uri = "{$parts['scheme']}://{$parts['host']}" . ( isset( $parts['port'] ) ? ':' . $parts['port'] : '' ) . add_query_arg( null, null );
+
+		return $current_uri;
+}

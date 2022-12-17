b0cde1938be9f657c3d94355ec9e3c16ea5185f1
Younes DRO <dro66_8@hotmail.fr>
2022-12-17 06:59:56 +0100

    #10

--- includes/class-connect-profilepress-and-discord.php
+++ includes/class-connect-profilepress-and-discord.php
@@ -174,6 +174,7 @@ class Connect_Profilepress_And_Discord {
 		$this->loader->add_action( 'admin_post_profilepress_discord_save_role_mapping', $plugin_admin, 'ets_profilepress_discord_save_role_mapping' );
 		$this->loader->add_action( 'admin_post_profilepress_discord_save_advance_settings', $plugin_admin, 'ets_profilepress_discord_save_advance_settings' );
 		$this->loader->add_action( 'admin_post_profilepress_discord_save_appearance_settings', $plugin_admin, 'ets_profilepress_discord_save_appearance_settings' );
+		$this->loader->add_action( 'admin_post_profilepress_discord_send_support_mail', $plugin_admin, 'ets_profilepress_discord_send_support_mail' );
 
 	}
 
